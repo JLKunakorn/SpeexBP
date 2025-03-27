4d9a3086da435b84bafa292a0abddb6dbad01656
JLKunakorn <157047365+JLKunakorn@users.noreply.github.com>
2025-03-27 13:35:19 +0700

    Update Start.js

--- Start.js
+++ Start.js
@@ -1,8 +1,2 @@
-const button = document.querySelector('button.call-to-action:not(.ml-auto)');
-
-if (button) {
-  button.click();
-  console.log('คลิกปุ่ม "เริ่มตอนนี้" ที่ไม่มี ml-auto แล้ว');
-} else {
-  console.log('ไม่พบปุ่ม "เริ่มตอนนี้" ที่ไม่มี ml-auto');
-}
+const btn = document.querySelector('.call-to-action:not(.ml-auto)'); 
+if (btn) btn.click();
