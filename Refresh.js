2eb41e9ecc5ff2a3aaf23d1a97eccb8fc68c5c79
JLKunakorn <157047365+JLKunakorn@users.noreply.github.com>
2025-04-21 18:33:30 +0700

    Update Refresh.js

--- Refresh.js
+++ Refresh.js
@@ -51,7 +51,7 @@ async function doCorrection(delay = 500) {
 }
 
 // 3. แก้ไขเฉพาะข้อผิด
-async function fixIncorrect(loopDelay = 100, maxRetries = 10) {
+async function fixIncorrect(loopDelay = 10, maxRetries = 20) {
   let tries = 0;
   while (tries < maxRetries) {
     const wrongs = document.querySelectorAll(".input-group.has-error");
